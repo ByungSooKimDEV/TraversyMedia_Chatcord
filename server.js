7043f190604f3cf9dc2ce5c9cb5312e9ce2f49ba
ByungSooKimDEV <byungsoo.dev@gamil.com>
2023-07-22 14:16:37 +0900

    [ADD] 기본 채팅 구현

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
+const formatMessage = require('./utils/message');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+const botName = 'ChatCord Bot';
+
+// Run when client connect
+io.on('connection', (socket) => {
+	// Welcome current user
+	socket.emit('message', formatMessage(botName, 'Welcome to ChatCord!'));
+
+	// Broadcast when a user connects
+	socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the chat'));
+
+	// Run when client discconects
+	socket.on('disconnect', () => {
+		io.emit('message', formatMessage(botName, 'A user has left the chat'));
+	});
+
+	// Listen for chatMessage
+	socket.on('chatMessage', (msg) => {
+		io.emit('message', formatMessage('USER', msg));
+	});
+});
+
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
